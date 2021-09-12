d47d78b2b1fb32919cc1775fc00422021e4ad4e7
Yang <31609073+GlobalYZ@users.noreply.github.com>
2021-09-12 16:11:38 +0800

    Update Program.cs

--- 005-Action委托/Program.cs
+++ 005-Action委托/Program.cs
@@ -27,9 +27,10 @@ namespace _005_Action委托 {
         }
         static void Main(string[] args)
         {
+            //Action声明的委托类型，是永远没有返回值的，通过在Action后边的<泛型>来指定参数的类型。
             //Action a = PrintString;//action是系统内置（预定义）的一个委托类型，它可以指向一个没有返回值，没有参数的方法
             //Action<int> a=PrintInt;//定义了一个委托类型，这个类型可以指向一个没有返回值，有一个int参数的方法
-            //Action<string> a = PrintString;//定义了一个委托类型，这个类型可以指向一个没有返回值，有一个string参数的方法 在这里系统会自动寻找匹配的方法
+            //Action<string> a = PrintString;//定义了一个委托类型，这个类型可以指向一个没有返回值，有一个string参数的方法 在这里系统会在同名方法中，自动寻找匹配的方法
             Action<int, int> a = PrintDoubleInt;
             a(34, 23);
             Console.ReadKey();
